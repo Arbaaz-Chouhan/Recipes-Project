f95af131a74a80210e4a96568936103df7cc8d44
Adnan-chouhan <adnanchouhan@gmail.com>
2024-06-03 18:13:26 +0530

    make recipes project

---
+++ js/aap.js
@@ -0,0 +1,17 @@
+const getElement =(selector) => {
+    const element = document.querySelector(selector)
+    if(element) return  element
+    throw Error(
+       `please  double ckeck class names,there is no ${selector}
+       class`
+    )
+}
+
+const links = getElement('.nav-links')
+const navBtnDOM = getElement('.nav-btn')
+
+navBtnDOM.addEventListener('clik',() => {
+    links.classList.toggle('show-links')
+})
+
+
